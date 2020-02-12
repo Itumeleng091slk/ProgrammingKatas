a75635bd9eb9cb88988b3a0a1ae4b23d32966430
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-02-12 15:03:54 +0200

    Update Frame.py

--- Frame.py
+++ Frame.py
@@ -1,10 +1,10 @@
 #Exercise: Frame some text
-def Frame(words) : 
-    s = "write code everyday"
+
+test = "write code everyday"
+def frame(words): 
     size = len(max(words, key=len))
     print('*' * (size + 4))  
     for word in words:
-          print('* {a:<{b}} *'.format(a=word, b=size))
+        print('* {a:<{b}} *'.format(a=word, b=size))
     print('*' * (size + 4))
-
-print(Frame(s.spilt(" "))
+print(frame(test.split(" ")))
