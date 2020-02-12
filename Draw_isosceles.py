d8e90eee56ab283bf4b4d31c9b3537937db81d26
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-02-12 14:47:56 +0200

    Create Draw_isosceles.py

---
+++ Draw_isosceles.py
@@ -0,0 +1,18 @@
+#Exercise: Draw an isosceles triangle
+#Isosceles (2)
+def isosceles(number_row):
+    isosceles_row = ''
+    number_row = 2*number_row - 2
+    
+    for count_row in range(0,number_row):
+        for column in range (0, number_row):
+            print(end=" ")
+        number_row = number_row -1
+        for column in range(0, count_row+1):    
+            print("#", end=" ") 
+        print("\r")
+    return isosceles_row 
+
+test1 = isosceles(2)
+
+print(test1)
