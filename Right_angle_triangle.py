6d5cb654fa9c891e277815df219a0e1a79a5f3a9
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-02-12 14:46:28 +0200

    Update Right_angle_triangle.py

--- Right_angle_triangle.py
+++ Right_angle_triangle.py
@@ -6,3 +6,12 @@ def triangle(number_rows):
     return triangle_size
 test1 = triangle(2)
 print(test1)
+
+# Exercise: Draw a right handed triangle
+def triangle(number_rows):
+    triangle_size = ''
+    for row_count in range(abs(number_rows) + 1):
+        print("#" * row_count)
+    return triangle_size
+test1 = triangle(4)
+print(test1)
