4837882e6797ba5f8e8dae0e95dff9016ed24222
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-02-12 14:41:11 +0200

    Update hellocode.py

--- hellocode.py
+++ hellocode.py
@@ -1,1 +1,7 @@
+#Excercise: Hello/removed line that takes an input
+ def Hello(person_name):
+     greet = "Hello" + " " + person_name + "!"
+     return greet
 
+result = Hello ("Tshepo")
+print(result)
