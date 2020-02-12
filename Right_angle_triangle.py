c663bf657244f9a5f4b812902349567c4ce5b6bc
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-02-12 14:45:38 +0200

    Update Right_angle_triangle.py

--- Right_angle_triangle.py
+++ Right_angle_triangle.py
@@ -1,1 +1,8 @@
-
+# Exercise: Draw a right angle triangle/removed line that takes an input
+def triangle(number_rows):
+    triangle_size = ''
+    for row_count in range(abs(number_rows) + 1):
+        print("#" * row_count)
+    return triangle_size
+test1 = triangle(2)
+print(test1)
