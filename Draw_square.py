0362507cc3177e79d361ef3ee6573392dc4304de
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-02-12 14:44:22 +0200

    Update Draw_square.py

--- Draw_square.py
+++ Draw_square.py
@@ -1,1 +1,13 @@
-
+# Exercise: Draw a square/removed a line to take an input
+def square(number_rows):
+    square_form = ''
+    
+    for row in range(number_rows):
+        for coloumn in range(number_rows):
+            print('#',  end="")
+        print()
+    return square_form
+test1 = square(2)
+test2 = square(4)
+print(test1)
+print(test2)
