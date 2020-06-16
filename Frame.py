7d90bd6ad814fc875238bbfb85e4950a17776b7b
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-06-16 14:30:29 +0200

    Update Frame.py

--- Frame.py
+++ Frame.py
@@ -1,10 +1,26 @@
 #Exercise: Frame some text
 
-test = "write code everyday"
-def frame(words): 
-    size = len(max(words, key=len))
-    print('*' * (size + 4))  
-    for word in words:
-        print('* {a:<{b}} *'.format(a=word, b=size))
-    print('*' * (size + 4))
-print(frame(test.split(" ")))
+def frame_code(words):
+    len_words = len(words) #find the longest word first
+    longest = words[0]
+    for i in range(len_words):
+        if len(words[i]) >= len(longest):
+            longest = words[i]
+
+    len_longest = len(longest)
+    len_row = len_longest + 4
+    return "*"*len_row
+    print("*"*len_row)
+
+    for i in range (len_words):
+        word_row = "* " + words[i]
+        if len(word_row) < len_row + 2:
+            while len(word_row) < len_row-1:
+                word_row = word_row + " "
+            word_row = word_row + "*"
+            print(word_row)
+            continue
+        word_row = word_row + " *"
+        print(word_row)
+    return "*"*len_row
+    print("*"*len_row) 
