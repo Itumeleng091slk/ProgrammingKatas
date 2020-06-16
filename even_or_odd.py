dab824a23b0058acc1b35709f182843b9b2f086d
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-06-16 14:25:27 +0200

    Update even_or_odd.py

--- even_or_odd.py
+++ even_or_odd.py
@@ -1,10 +1,9 @@
 #Exercise: check if a number is even/removed line to take an input
 def even_or_odd(number):
-    even_result = "{0} is even" .format(number)
-    odd_result = "{0} is odd" .format(number)
-    if (number % 2) == 0:
-        return even_result  
+    if(number % 2 == 0):
+        return "even"
     else:
-        return odd_result        
-look = even_or_odd(2) # Look function to check/look whether the given integer is even or odd
-print(look)
+        return "odd"     
+result = even_or_odd(2) # Look function to check/look whether the given integer is even or odd
+print(result)
+
