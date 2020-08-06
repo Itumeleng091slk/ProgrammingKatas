c88469a7564cfc0a644d12878331291ee7b75a82
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-08-06 12:51:20 +0200

    removed unused variable

--- Draw_isosceles.py
+++ Draw_isosceles.py
@@ -4,9 +4,7 @@ def isosceles(number_row):
     for count_row in range(1,number_row + 1):
         print(f' ' * (number_row - count_row) + '#' * isosceles_row)
         isosceles_row += 2
-    return 'printed isoceles trinagle'
 
 if __name__ == "__main__":
     isosceles(2)
     isosceles(4)
-    
